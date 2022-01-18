db4f9169e70f0749052460571f8771ba78c538a2
r45 <fabienleclerc11@yahoo.fr>
2022-01-18 22:18:37 +0100

    ajout de la fonctionnalite scene pour gérer plusieurs spheres

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+#include <list>
+
+#include <iostream>
+
 
 
 
@@ -75,15 +79,18 @@ Vector C,u;
  
 class Sphere {
 public:
-	Sphere( const Vector& O, const double R) : O(O),R(R){
+	Sphere( const Vector& O, const double R, const Vector& rho) : O(O),R(R),rho(rho){
 
 };
 
-	bool intersect(const Ray& r , Vector& P, Vector& N) {
+	
+
+	bool intersect(const Ray& r , Vector& P, Vector& N, double& t) {
 		//solve a*t^2 +b*t +c = 0
 		double a =1;
 		double b= 2*dot(r.u , r.C - O );
 		double c= (r.C - O).norm2()- R*R;
+
 
 		double delta = b*b - 4*a*c;
 		if (delta < 0){
@@ -96,7 +103,7 @@ public:
 			return false;
 		}
 
-		double t;
+
 		if (t1>0){
 			t = t1;
 		}
@@ -113,6 +120,41 @@ public:
 	}
 		double R=R;
 		Vector O=O;
+		Vector rho=rho;
+
+};
+
+class Scene{
+	public:
+	Scene(){};
+	void addSphere(const Sphere& s) {spheres.push_back(s);}
+
+	bool intersect_scene(const Ray& r , Vector& P, Vector& N, int &rang_sphere){
+
+		bool inter_scene = false;
+		double t_min=1E99;
+
+		for (int i=0;i<spheres.size();i++){
+			Vector P_sphere, N_sphere;
+			double t;
+			bool inter_sphere = spheres[i].intersect(r,P_sphere,N_sphere,t);
+			if(inter_sphere){
+				inter_sphere=true;
+				if(t<t_min){
+					t_min=t;
+					P = P_sphere;
+					N= N_sphere;
+					rang_sphere=i;
+				}
+			}
+		}
+
+		return inter_scene;
+
+	}
+
+	std::vector<Sphere> spheres;
+	
 
 };
  
@@ -122,14 +164,31 @@ int main() {
 	int H = 512;
 	Vector C(0,0,55);
 
-	Sphere s(Vector(0,0,0),10);
+	Sphere s1(Vector(0,0,0),10,Vector(0,1,0)); //albedo vert (couleur de la sphère)
+	Sphere s2(Vector(-10,-10,-20),15,Vector(1,0,0)); //albedo rouge (couleur de la sphère)
+
+	std::vector<Sphere> spheres;
+	spheres.insert(spheres.begin(),s1);
+	spheres.insert(spheres.begin(),s2);
+
+	
+
+	Scene scene;
+	scene.addSphere(s1);
+	scene.addSphere(s2);
+
+
 	double fov=60*3.14/180;
 	double tanfov2 = tan(fov/2);
 
 	Vector lumiere_position(-10,20,40);
-	double intensite_lumiere = 3000000;
+	double intensite_lumiere = 6000000;
 
-	Vector rho(1,0,0); //albedo rouge (couleur de la sphère)
+	//std::cout << spheres[0].rho[1];
+
+	//Vector rho(1,0,0); 
+
+	
 
 	std::vector<unsigned char> image2(W * H * 3, 0);
 	for (int i = 0; i < H; i++) {
@@ -138,21 +197,24 @@ int main() {
 			Vector u(j-W/2+0.5, H-i-H/2+0.5,-W/(2*tanfov2));
 			u.normalize();
 			Ray r(C,u);
-			Vector P,N; 
-			bool inter = s.intersect(r,P,N);
+			Vector P,N;
+			int rang_sphere;
+
+			int inter = scene.intersect_scene(r,P,N,rang_sphere);
+	
 
 			double intensite_pixel=0.;
 
 			Vector l = (lumiere_position - P);
 			l.normalize();
 
-
-			if (inter){
+			
+			if (inter != -1){
 				intensite_pixel = intensite_lumiere * std::max(0.,dot(l,N)) / (4 * M_PI * (lumiere_position - P).norm2());
 				
-				image2[(i * W + j) * 3 + 0] = std::min(255.,std::max(0.,intensite_pixel))*rho[0];
-				image2[(i * W + j) * 3 + 1] = std::min(255.,std::max(0.,intensite_pixel))*rho[1];
-				image2[(i * W + j) * 3 + 2] = std::min(255.,std::max(0.,intensite_pixel))*rho[2];
+				image2[(i * W + j) * 3 + 0] = std::min(255.,std::max(0.,intensite_pixel))*spheres[rang_sphere].rho[0];
+				image2[(i * W + j) * 3 + 1] = std::min(255.,std::max(0.,intensite_pixel))*spheres[rang_sphere].rho[1];
+				image2[(i * W + j) * 3 + 2] = std::min(255.,std::max(0.,intensite_pixel))*spheres[rang_sphere].rho[2];
 			}
 		
 
